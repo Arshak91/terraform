0a4d3407025ea342dad8af6e214440fbb950bb80
Arshak Gasparyan <arshakgasparyan1@gmail.com>
2023-05-02 00:30:58 +0400

    changes

--- ec2/instance.tf
+++ ec2/instance.tf
@@ -1,7 +1,7 @@
 
 variable "private_key_path" {
   type    = string
-  default = "./../../../../Downloads/virginia.pem"
+  default = "./../../../pem/virginia.pem"
 }
 variable "instance_type" {
   type    = string
@@ -32,17 +32,28 @@ resource "aws_instance" "web" {
       host        = self.public_ip
     }
     inline = [
+      # install NGNIX
       "sudo apt update",
       "sudo apt install nginx -y",
       "sudo systemctl enable nginx",
-
+      # install Jenkins
       "sudo apt install openjdk-11-jdk -y",
       "sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
       "sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
       "sudo apt update",
       "sudo apt install jenkins -y",
       "sudo systemctl start jenkins",
-      "sudo systemctl enable jenkins"
+      "sudo systemctl enable jenkins",
+      # install terraform
+      "wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg",
+      "gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint",
+      "echo \"deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/hashicorp.list",
+      "sudo apt update",
+      "sudo apt install terraform -y",
+
+      "sudo apt update",
+      "sudo scp -o StrictHostKeyChecking=no -i ${file(var.private_key_path)} ${file(var.private_key_path)} ubuntu@${self.public_dns}:/home/ubuntu"
+
     ]
   }
   tags = {
--- ec2/s3backend.tf
+++ ec2/s3backend.tf
@@ -6,3 +6,11 @@ terraform {
     encrypt        = true
   }
 }
+# terraform {
+#   backend "s3" {
+#     bucket         = "myterraform-1"
+#     key            = "global://s3/terraform.tfstate"
+#     region         = "us-east-1"
+#     encrypt        = true
+#   }
+# }
